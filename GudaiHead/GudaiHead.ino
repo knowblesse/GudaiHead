5640ac8b6afc5f9854915239d4e3eb3f45836071
Knowblesse <knowblesse@gmail.com>
2023-04-17 16:32:29 +0900

    No Control. Just moving randomly

--- GudaiHead/GudaiHead.ino
+++ GudaiHead/GudaiHead.ino
@@ -89,6 +89,7 @@ void setTwoMotorState(double speed, int motor1P, int motor1D, bool motor1F, int 
   }
 }
 
+unsigned long startMovingTime;
 void setup() {
 
   Serial.begin(9600);
@@ -105,6 +106,12 @@ void setup() {
 
   pinMode(BR_P, OUTPUT);
   pinMode(BR_D, OUTPUT);
+
+  delay(8000);
+  goUp();
+  setSpeed(180);
+  startMovingTime = millis();
+
 }
 
 double readValue;
@@ -118,76 +125,19 @@ unsigned long timeout = 1000;
 int x, y, z;
 int* remoteValue[3];
 
+bool movemove = true; // for habituation
+
+bool isMovingUp = false;
+unsigned long randomTime = random(10000);
 void loop() 
 {
-  uint8_t temp;
-  uint8_t buffer[8];
-  if(mySerial.available())
-  {
-    temp = mySerial.read(); // read first two byte and confirm 0xFF
-    if(temp == 0xFF)
-    {
-      while(!mySerial.available()){}
-      temp = mySerial.read();
-      if(temp == 0xFF)
-      {
-        mySerial.readBytes(buffer, 8);
-        
-        if(buffer[6] == 0xFF & buffer[7] == 0xFF) // read last two byte and confirm 0xFF
-        {
-          // intact signal received.
-          lastSerialTime = millis();
-
-          // parse values
-          x = buffer[0] | buffer[1]<<8;
-          y = buffer[2] | buffer[3]<<8;
-          z = buffer[4] | buffer[5]<<8;
-
-          Serial.print(x);
-          Serial.print(',');
-          Serial.print(y);
-          Serial.print(',');
-          Serial.print(z);
-          Serial.println("");
-
-          // transform z to -1 ~ +1
-          if (z < Z_default) z = -(Z_default - z) / Z_default;
-          else if (z > Z_default) z = (z - Z_default) / (1023 - Z_default);
-          else z = 0;
-
-          // First check the rotation
-          if (abs(z) > speed_limit)
-          {
-            // rotate
-            setTwoMotorState(z, FR_P, FR_D, FR_F, BR_P, BR_D, BR_F);
-            setTwoMotorState(-z, FL_P, FL_D, FL_F, BL_P, BL_D, BL_F);
-            return;
-          }
-
-          // If no rotation is signaled, go movement
-          //change x, y range to -1 ~ +1
-          if (x < X_default) x = -(X_default - x) / X_default;
-          else if (x > X_default) x = (x - X_default) / (1023 - X_default);
-          else x = 0;
-
-          if (y < Y_default) y = -(Y_default - y) / Y_default;
-          else if (y > Y_default) y = (y - Y_default) / (1023 - Y_default);
-          else y = 0;
-
-          // Rotate dimension
-          slash_motor = 0.7071 * (-x + y) / speed_max;
-          backslash_motor = 0.7071 * (x + y) / speed_max;
-
-          // Change motor state
-          setTwoMotorState(slash_motor, FR_P, FR_D, FR_F, BL_P, BL_D, BL_F);
-          setTwoMotorState(backslash_motor, FL_P, FL_D, FL_F, BR_P, BR_D, BR_F);
-
-        }
-      }
-    }
-  }
-  if (lastSerialTime + timeout < millis())
-  {
-      setSpeed(0);
-  } 
+  randomTime = random(3000,5000);
+  delay(randomTime);
+  
+  randomTime = random(3000,5000);
+  setSpeed(0);
+  delay(randomTime);
+  goUp();
+  setSpeed(180);
+  
 }
