a4416cf16e8a963e44203e15e45f95e49132c18d
Knowblesse <knowblesse@gmail.com>
2023-04-30 21:27:47 +0900

    Integrated AT command part in the remote controller

--- RemoteControl/RemoteControl.ino
+++ RemoteControl/RemoteControl.ino
@@ -16,7 +16,60 @@ int z;
 
 double slash_motor;
 double backslash_motor;
+void sendATCommand()
+{
+  for(int i = 0; i < 3; i++)
+  {
+    unsigned long startTime = millis();
+    unsigned long timeout = 6000;
+    unsigned long temp;
 
+    Serial1.flush();
+    delay(1000); // Go slience for one sec
+    
+    Serial.println("Entering the AT mode");
+    Serial1.write("+++"); // Entering the AT mode
+    temp = millis();
+    
+    while (!Serial1.available() && (millis() - startTime) < timeout){} // wait until zigbee issue "OK\r"
+    if (!Serial1.available())
+    {
+      Serial.println("timeout");
+      continue;
+    }
+    else
+    {
+      Serial.println(millis() - temp);
+      Serial.println(Serial1.readStringUntil('\r'));
+    }
+    
+    // Now Send AT commands
+    while(Serial.available())
+    {
+      Serial1.write(Serial.read()); // write AT command
+    } 
+    Serial1.write('\r'); // write carrage return (set "No Line Ending" for the Serial monitor)
+    
+    // Wait Response
+    while (!Serial1.available() && (millis() - startTime) < timeout){} // wait until zigbee issue "OK\r"
+    if (!Serial1.available())
+    {
+      Serial.println("timeout");
+      continue;
+    }
+    else
+    {
+      while(Serial1.available())
+      {
+        Serial.print(Serial1.readString());
+      }
+    }
+    Serial.println(' ');
+    Serial.println("response received");
+    Serial1.write("ATCN\r");
+    return;
+  }
+}
 void loop()
 {
   x = analogRead(PIN_X);
@@ -31,16 +84,15 @@ void loop()
   Serial1.write(0xFF);
   Serial1.write(0xFF);
   
-  if(Serial1.available())
+  if(Serial.available())
   {
-    //Serial.println(Serial1.readString());
+    sendATCommand();
   }
-  Serial.print(x);
-  Serial.print(",");
-  Serial.print(y);
-  Serial.print(",");
-  Serial.println(z);
+  // Serial.print(x);
+  // Serial.print(",");
+  // Serial.print(y);
+  // Serial.print(",");
+  // Serial.println(z);
   delay(20);
-
 }
 
